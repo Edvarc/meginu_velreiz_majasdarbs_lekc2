5d102fcc0784c3ab75a11eb7aeee189616a6b782
Edvarc <89932538+Edvarc@users.noreply.github.com>
2021-09-15 14:57:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            console.writeline ("Edvarc testē raksta tekstu tagad iekš github.. jautājums ir, kur un kā šo kodu var attēlot kā izpildītu, t.i., parādīt tikai šo teikumu");
+        }
+    }
+}
