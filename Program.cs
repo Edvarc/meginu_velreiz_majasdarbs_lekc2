9286cff3f2769fc23c182a30344efd1ef76d18c8
Edvarc <89932538+Edvarc@users.noreply.github.com>
2021-09-15 15:54:07 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,12 +8,12 @@ namespace meginu_velreiz_MD_nr2_8_sept
         {
             //  1. UZDEVUMS --------------------------------------------------------------
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Kā Tevi sauc? Lūdzu, norādi lietotājvārdu:");
+            Console.WriteLine("1. UZDEVUMS.  Kā Tevi sauc? Lūdzu, norādi lietotājvārdu:");
             string usn = Console.ReadLine();
             Console.WriteLine("Sveiks," + usn + "!");
 
             //  2. UZDEVUMS --------------------------------------------------------------
-            Console.WriteLine("Kāds ir Tavs vecums?");
+            Console.WriteLine("2. UZDEVUMS. Kāds ir Tavs vecums?");
             string age = Console.ReadLine();
             int age2 = int.Parse(age);
             int age3 = age2 + 1;
@@ -21,7 +21,7 @@ namespace meginu_velreiz_MD_nr2_8_sept
 
             //  3. UZDEVUMS --------------------------------------------------------------
 
-            Console.WriteLine("Uzraksti, lūdzu, vienu veselu skaitli:");
+            Console.WriteLine("3. UZDEVUMS. Uzraksti, lūdzu, vienu veselu skaitli:");
             string num1 = Console.ReadLine();
             int num1p = int.Parse(num1);
             Console.WriteLine("Paldies! Uzraksti, lūdzu, vēl vienu veselu skaitli un tad es pateikšu, kurš no tiem ir vislielākais! :)");
@@ -35,9 +35,33 @@ namespace meginu_velreiz_MD_nr2_8_sept
             //tests 15 sept 
 
 
-           
+            // 5. UZDEVUMS -------------------------------
+
+            Console.WriteLine("5. UZDEVUMS. Uzraksti, lūdzu, skaitli no 50 līdz 100!");
+            string cipar1 = Console.ReadLine();
+            int cipar1p = int.Parse(cipar1);
+            Console.WriteLine("Paldies! Uzraksti, lūdzu, vēl vienu skaitli - no 10 līdz 20! Un pēc tam uzzināsi abu skaitļu dalījuma atlikumu!");
+            string cipar2 = Console.ReadLine();
+            int cipar2p = int.Parse(cipar2);
+            int atlikums = cipar1p % cipar2p;
+            Console.WriteLine("Atlikums:    " + atlikums);
 
 
+
+            //7. uzdevums - Taisnstūra laukuma aprēķināšana
+            //Iegūt no lietotāja taisnstūra malu vērtības un aprēķināt taisnstūra laukumu.
+            // Precīzi jāaprēķina laukums arī ja tas sanāk decimāldaļās jānoapaļo ar 2 cipariem aiz komata
+
+
+            Console.WriteLine("7. UZDEVUMS. Uzraksti, lūdzu, skaitli no 1 līdz 10!");
+            string rect1 = Console.ReadLine();
+            decimal rect1p = decimal.Parse(rect1);
+            Console.WriteLine("Paldies! Uzraksti, lūdzu, vēl vienu skaitli - no 10 līdz 20! Un pēc tam uzzināsi taisnstūra, kura malu garumi ir Tevis tikko norādītie skaitļi, laukumu kv. cm!");
+            string rect2 = Console.ReadLine();
+            decimal rect2p = decimal.Parse(rect2);
+            decimal laukums = rect1p * rect2p;
+            decimal apalots = Math.Round(laukums, 2);
+            Console.WriteLine("Laukums =    " + apalots);
         }
     }
 }
